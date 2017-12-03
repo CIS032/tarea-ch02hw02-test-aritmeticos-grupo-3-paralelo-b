5e3c2a701c32d07443be9ec1e858393d51a74410
Maria Jose <33472986+Majo96Salazar@users.noreply.github.com>
2017-12-03 13:37:43 -0500

    Add files via upload

---
+++ PreguntaAdicion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testpreguntasaritmeticas;
+
+/**
+ *
+ * @author Grupo 3
+ */
+public class PreguntaAdicion implements PreguntaEntera{
+
+    private int a, b;//Los numeros del problema
+
+    public PreguntaAdicion()  {//constructor
+        a = (int) (Math.random() * 50 + 1);
+        b = (int) (Math.random() * 50);
+    }
+
+    public String getPregunta() {
+        return "Cuanto es: " + a + " + " + b + " ?";
+    }
+
+    public int getRespuestaCorrecta() {
+        return a + b;
+    }
+}
